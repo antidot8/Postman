7ed9d1560f2ec7fcbfc318a05b892c4aa1ca004f
Alexander Siluyanov <antidot8@mail.ru>
2021-06-16 17:34:05 +0400

    Build should passed (Change indent in test)

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -10,13 +10,14 @@ public class PostmanEchoTest {
     @Test
     void shouldPostSend() {
         given()
-                .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("данные")
-                .when()
-                .post("/post")
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("данные"));
+            .baseUri("https://postman-echo.com")
+            .contentType("text/plain; charset=UTF-8")
+            .body("данные")
+        .when()
+            .post("/post")
+        .then()
+            .statusCode(200)
+            .body("data", equalTo("данные"))
+        ;
     }
 }
